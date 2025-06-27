3caec82087744fce4e459230b7460e288e29b298
Muhammet Ali Tanrıkulu <muhammedd45@icloud.com>
2025-06-27 19:41:51 +0300

    ..

---
+++ Presentation/UdemyCarBook.WebAPI/Controllers/LocationController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.Mediator.Commands.LocationCommands;
+using UdemyCarBook.Application.Features.Mediator.Queries.LocationQueries;
+
+namespace UdemyCarBook.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LocationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LocationList()
+        {
+            var values = await _mediator.Send(new GetLocationQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocationById(int id)
+        {
+            var values = await _mediator.Send(new GetLocationByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Güncelleme Başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteLocation(int id)
+        {
+            await _mediator.Send(new RemoveLocationCommand(id));
+            return Ok("Silme Başarılı");
+        }
+    }
+}
---
+++ Presentation/UdemyCarBook.WebAPI/Controllers/PricingController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.Mediator.Commands.PricingCommands;
+using UdemyCarBook.Application.Features.Mediator.Queries.PricingQueries;
+
+namespace UdemyCarBook.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PricingController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PricingList()
+        {
+            var values = await _mediator.Send(new GetPricingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPricingById(int id)
+        {
+            var values = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Güncelleme Başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok("Silme Başarılı");
+        }
+    }
+}
