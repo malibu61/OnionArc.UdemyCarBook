9e789fb52e0a23731424736355410813b66fe0a3
Muhammet Ali Tanrıkulu <muhammedd45@icloud.com>
2025-07-11 14:19:49 +0300

    ..

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.CarPricingDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardCarPricingListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdminDashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.v1 = "Paketler";
+            ViewBag.v2 = "Araç Fiyat Paketleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7274/api/CarPricing/GetCarsWPricingAndBrandWithTimePeriod");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarsWPricingAndBrandBYTimePeriodDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardChart2ComponentPartial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.Dto.CarDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardChart2ComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardChart2ComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7274/api/Car/CarListWBrand");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new List<ResultCarBrandChartDto>());
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCarBrandChartDto>>(json);
+
+            var grouped = values
+                .GroupBy(x => x.BrandName)
+                .Select(g => new ResultCarBrandChartDto
+                {
+                    BrandName = g.Key,
+                    Count = g.Count()
+                }).ToList();
+
+            return View(grouped);
+        }
+    }
+}
